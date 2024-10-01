8de8d5e86828ccfa98d9e8b02b9d635291ec2930
KauaFSB <kauaflp234@gmail.com>
2024-10-01 20:33:12 -0300

    atividade lançada

---
+++ App.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet, View, TextInput, Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+
+export default function App() {
+  const [nome, setNome] = useState('')
+  const [nomeArmazenado, setNomeArmazenado] = useState('')
+
+  const salvarNome = async () => {
+    try {
+      await AsyncStorage.setItem('@nome_usuario', nome)
+      alert('Nome salvo com sucesso!')
+      carregarDados()
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+  const carregarDados = async () => {
+    try {
+      const valor = await AsyncStorage.getItem('@nome_usuario')
+      if(valor != null){
+        setNomeArmazenado(valor)
+      }
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+  useEffect( () => {
+    carregarDados()
+}, [])
+
+  return (
+   <View style={styles.container}>
+      <Text>Nome armazenado: {nomeArmazenado}</Text>
+      <TextInput
+       placeholder='Digite seu nome'
+       value={nome}
+       onChangeText={setNome}
+       style={styles.input}
+      />
+      <Button title='salvar nome'
+      onPress={salvarNome}/>
+   </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10
+  }
+});
